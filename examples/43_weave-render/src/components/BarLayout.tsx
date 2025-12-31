3fc9b2d3e059ba339ba57f7ff539ac9732e96ab5
Master-Hash <hash@hash.moe>
2025-12-31 23:22:54 +0800

    ci: setup and test
    style: use `use` instead of `useContext`
    
    feat: Navigation API Router
    
    ci: fix
    
    style: `#private`
    
    fix: no changeRoute after abortion
    
    fix: simplify abortion when navigation
    
    chore doc
    
    fix: 404 when no 404.tsx?
    
    fix: 404 when no 404.tsx?
    
    fix: lint
    
    fix: remove flushSync workaround, now 404 client navigation is working
    
    fix: 404 (hopefully final)
    
    fix: redirection, but not redirection to 404
    
    fix: Dynamic not found sync & async
    
    fix: network error
    
    fix: fs-router › encoded path
    
    fix: ssr redirect with WORKAROUND
    
    fix: useRouter(?)
    
    fix: remove all unstable pending
    
    fix: use-router > updates hashes
    
    fix: route change event handlers
    
    chore!: won't fix https://github.com/wakujs/waku/issues/1255
    
    chore!: remove unstable pending from fixtures
    
    refactor: merge client and server error
    
    fix: regression about committing url before dom mounted
    
    fix: history entry when redirection
    
    fix: always cache static rsc with encoded URI
    
    test: add race-condition e2e
    
    remove `routeInterceptor`
    
    revert encodeURI
    
    fix: use `controller.redirect` instead of another push
    
    fix: redirect (again)
    
    fix: style
    
    fix: redirect on action
    
    chore: add FIXME on redirect history
    
    fix: workaround about redirect
    
    test ?
    
    fix: use a resolve state for client error cases where route not changes
    
    also align with popular approach
    
    fix: inital my workaround about redirect
    
    fix: ssr-catch-error regression with WORKAROUND
    
    A better and complete fix
    
    fix ssr
    
    fix: correct redirect
    
    Revert "fix: use a resolve state for client error cases where route not changes"
    
    This reverts commit 5418e6e593327a96903320863b8f7592c9667d17.
    
    fix: never flush when client error
    
    a better way to handle reset for redirect
    
    signal 也许不需要通过参数传递？
    
    Revert "signal 也许不需要通过参数传递？"
    
    This reverts commit 91e64221bf3fcc8f314ebaafdbe2daa7c867d9a5.
    
    (partly revert) 强制使用 startTransition
    
    Revert "(partly revert) 强制使用 startTransition"
    
    This reverts commit 5f7c21d60e7832852be7fb57b2664607be8cd2d0.
    
    signal 也许不需要通过参数传递？

--- examples/43_weave-render/src/components/BarLayout.tsx
+++ examples/43_weave-render/src/components/BarLayout.tsx
@@ -3,49 +3,19 @@ import { Link } from 'waku/router/client';
 
 import '../styles.css';
 
-const Pending = ({ isPending }: { isPending: boolean }) => (
-  <span
-    style={{
-      marginLeft: 5,
-      transition: 'opacity 75ms 100ms',
-      opacity: isPending ? 1 : 0,
-    }}
-  >
-    Pending...
-  </span>
-);
-
 const BarLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
       <p>This Layout is expected to be static</p>
       <ul>
         <li>
-          <Link
-            to="/"
-            unstable_pending={<Pending isPending />}
-            unstable_notPending={<Pending isPending={false} />}
-          >
-            Home
-          </Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link
-            to="/foo"
-            unstable_pending={<Pending isPending />}
-            unstable_notPending={<Pending isPending={false} />}
-          >
-            Foo
-          </Link>
+          <Link to="/foo">Foo</Link>
         </li>
         <li>
-          <Link
-            to={'/nested/bar' as never}
-            unstable_pending={<Pending isPending />}
-            unstable_notPending={<Pending isPending={false} />}
-          >
-            Link to 404
-          </Link>
+          <Link to={'/nested/bar' as never}>Link to 404</Link>
         </li>
       </ul>
       {children}
